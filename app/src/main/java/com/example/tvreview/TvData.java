4347fca84012108920ab5337afe97b419f3ac037
Apoorva <apoorvaarya>
2020-10-04 15:50:47 -0400

    creating TvData

---
+++ app/src/main/java/com/example/tvreview/TvData.java
@@ -0,0 +1,38 @@
+package com.example.tvreview;
+
+public class TvData {
+    private String description;
+    private String itemName;
+    private String itemGenre;
+    private String itemRate;
+    private int itemImage;
+
+    public TvData(String itemName, String itemGenre, String itemRate, String description, int itemImage) {
+        this.itemName = itemName;
+        this.itemGenre = itemGenre;
+        this.itemRate = itemRate;
+        this.description = description;
+        this.itemImage = itemImage;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemGenre() {
+        return itemGenre;
+    }
+
+    public String getItemRate() {
+        return itemRate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getItemImage() {
+        return itemImage;
+    }
+
+}
