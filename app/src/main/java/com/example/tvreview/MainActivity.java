35b156337006b15b5cc7600a2d0289b33d81e2d9
Apoorva <apoorvaarya>
2020-10-04 16:32:28 -0400

    changes to Main Activity

--- app/src/main/java/com/example/tvreview/MainActivity.java
+++ app/src/main/java/com/example/tvreview/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.tvreview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    List<TvData> myTvList;
+    TvData myTvData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, 1);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+
+
+        myTvList = new ArrayList<>();
     }
 }
