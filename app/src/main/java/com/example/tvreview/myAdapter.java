f1b156296b88c74a0584ba925f55497942d82dcb
Apoorva <apoorvaarya>
2020-10-04 16:28:23 -0400

    changes to myAdapter

--- app/src/main/java/com/example/tvreview/myAdapter.java
+++ app/src/main/java/com/example/tvreview/myAdapter.java
@@ -1,6 +1,10 @@
 package com.example.tvreview;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -8,9 +12,39 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class myAdapter {
+import java.util.List;
 
+public class myAdapter extends RecyclerView.Adapter<TvViewHolder>{
 
+    private Context mContext;
+    private List<TvData> myTvList;
+
+    public myAdapter(Context mContext, List<TvData> myTvList) {
+        this.mContext = mContext;
+        this.myTvList = myTvList;
+    }
+
+    @NonNull
+    @Override
+    public TvViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View mView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_row_item, viewGroup, false);
+
+        return new TvViewHolder(mView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final TvViewHolder tvViewHolder, int i) {
+        tvViewHolder.imageView.setImageResource(myTvList.get(i).getItemImage());
+        tvViewHolder.mTitle.setText(myTvList.get(i).getItemName());
+        tvViewHolder.mGenre.setText(myTvList.get(i).getItemGenre());
+        tvViewHolder.mRate.setText(myTvList.get(i).getItemRate());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return myTvList.size();
+    }
 
 }
 
